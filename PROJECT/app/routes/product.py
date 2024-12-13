c3f8e99e268d0d18677f491d06ec78648df8fbe9
HarishNemade35 <harish.nemade@oorjaa.tech>
2024-12-13 11:25:10 +0530

    Update product.py

--- PROJECT/app/routes/product.py
+++ PROJECT/app/routes/product.py
@@ -15,6 +15,8 @@ async def add_product(
     product_data: ProductCreate, 
     db: Session = Depends(get_db),
     current_owner: Owner = Depends(get_current_owner)):
+
+    """Adds a new product to the database."""
     
     # Check if the product already exists
     existing_product = db.query(Product).filter(Product.name == product_data.name).first()
@@ -22,6 +24,7 @@ async def add_product(
         raise HTTPException(status_code=400, detail="Product already exists.")
     
 
+    # Create and add the product to the database
     # Create and add the product to the database
     product = Product(
         name=product_data.name,
@@ -41,12 +44,13 @@ async def add_product(
     )
 
 
-
+# Get All Products API
 @router.get("/get_product", response_model=List[ProductResponse])  # List of products
 async def get_all_product(
     db: Session = Depends(get_db),
     current_owner: Owner = Depends(get_current_owner)
 ):
+    """Fetches all products owned by the current owner."""
     if not current_owner:
         raise HTTPException(status_code=404, detail="Not authorized, this is only for Owner")
 
@@ -57,14 +61,14 @@ async def get_all_product(
     return products  # 
 
 
-
+# Delete Product API
 @router.delete("/product/{product_id}",status_code=200)
 async def delete_product(
     Product_int : int,
     db : Session = Depends(get_db),
     current_owner: Owner = Depends(get_current_owner)
 ):
-    
+    """Deletes a product owned by the current owner."""  
     product = db.query(Product).filter(Product.id == Product_int).first()
     
     #find order
@@ -72,6 +76,7 @@ async def delete_product(
         raise HTTPException(status_code=404, detail="Product not found")
     
     # Ensure the order belongs to the current user
+    # Ensure the product belongs to the current owner
     if not current_owner:
         raise HTTPException(status_code=403, detail="Not authorized to delete this product")
 
